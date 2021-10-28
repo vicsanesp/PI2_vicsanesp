fc0dcc702d2e1bcc894d40ccde0dfa0f42f0f8e4
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2021-10-28 21:44:57 +0200

    Update Ejercicio1.java

--- src/ejercicios/Ejercicio1.java
+++ src/ejercicios/Ejercicio1.java
@@ -7,8 +7,8 @@ import us.lsi.common.Files2;
 
 public class Ejercicio1 {
 
-//	Dada la siguiente definición recursiva de la función f (que toma como entrada 3
-//	números enteros positivos y devuelve una cadena):
+//	Dada la siguiente definiciÃ³n recursiva de la funciÃ³n f (que toma como entrada 3
+//	nÃºmeros enteros positivos y devuelve una cadena):
 //	  "(" + toString(a*b*c) + ")", a<3 and b<3 and c<3
 //	  
 //	  "(" + toString(a+b+c) + ")", a<5 or b<5 or c<5
@@ -16,13 +16,13 @@ public class Ejercicio1 {
 //	  toString(a*b*c) + f(a/2, b-2, c/2) a es par and b es par and c es par
 //	  
 //	  toString(a+b+c) + f(a/3, b-3, c/3) en otro caso
-//	siendo + un operador que representa la concatenación de cadenas, y toString(i) un método
-//	que devuelve una cadena a partir de un entero. Al llevar a cabo la implementación, para
+//	siendo + un operador que representa la concatenaciÃ³n de cadenas, y toString(i) un mÃ©todo
+//	que devuelve una cadena a partir de un entero. Al llevar a cabo la implementaciÃ³n, para
 //	el tratamiento de cadenas se recomienda hacer uso de String.format.
 	
 	public static String ejercicio1It(Integer a, Integer b, Integer c) {
-		
-		return null;
+		String cum = "";
+		return cum;
 	}
 	
 	public static String ejercicio1RecNoFin(Integer a, Integer b, Integer c, String cum) {
