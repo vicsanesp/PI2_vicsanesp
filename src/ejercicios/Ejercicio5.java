ca4edf2b5ffc759085d89738eac264bd65380388
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2021-11-05 13:39:50 +0100

    Update Ejercicio5.java

--- src/ejercicios/Ejercicio5.java
+++ src/ejercicios/Ejercicio5.java
@@ -1,29 +1,44 @@
 package ejercicios;
 
+import java.util.Map;
+import us.lsi.common.Map2;
 
 public class Ejercicio5 {
 
 //	hacer iterativo, y recursivo con y sin memoria de: 
-//			a + b^2 + 2*c, a<3 Ã³ b<3 Ã³ c<3
+//			a + b^2 + 2*c, a<3 ÃƒÂ³ b<3 ÃƒÂ³ c<3
 //
 //g(a,b,c)  g(a-1, b/2, c/2) + g(a-3, b/3, c/3),    a es multiplo de b
 //
 //			g(a/3, b-3, c-3) + g(a/2, b-2, c-2), eoc
 	
-//	public static Integer ejercicio5It(Integer a, Integer b, Integer c) {
-//		Integer res = 0;
-//		Integer[][][] g = null;
-//		if(a<3 || b<3 || c<3) {
-//			res = a + b*b + 2*c;
-//		}
-//		if(a%b == 0) {
-//			res = g[a-1][b/2][c/2] + g[a-3][b/3][c/3];
-//		}
-//		else {
-//			res = g[a/3][b-3][c-3] + g[a/2][b-2][c-2];
-//		}
-//		return res;
-//	}
+	private static record tripla( Integer a, Integer b, Integer c) {
+		public static tripla of(Integer a, Integer b, Integer c) {
+			return new tripla(a, b, c);
+		}
+	}
+	
+	public static Integer ejercicio5It(Integer a, Integer b, Integer c) {
+		Map<tripla, Integer> res = Map2.empty();
+		Integer cum = null;
+		for (int i = 0; i <= a; i++) {
+			for (int j = 0; j <= b; j++) {
+				for (int k = 0; k <= c; k++) {
+					if(i<3 || j<3 || k<3) {
+						cum = i + j*j + 2*k;
+					}
+					else if(i % j == 0) {
+						cum = res.get(tripla.of(i-1, j/2, k/2)) + res.get(tripla.of(i-3, j/3, k/3));
+					}
+					else {
+						cum = res.get(tripla.of(i/3, j-3, k-3)) + res.get(tripla.of(i/2, j-2, k-2));
+					}
+					res.put(tripla.of(i, j, k), cum);
+				}
+			}
+		}
+		return res.get(tripla.of(a, b, c));
+	}
 	
 	public static Integer ejercicio5RecNoMem(Integer a, Integer b, Integer c) {
 		if(a<3 || b<3 || c<3) {
