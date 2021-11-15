4a2e59788722f00a8751b9a54ee1d2cdbbee6766
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2021-11-15 15:22:06 +0100

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -20,6 +20,7 @@ public class Test {
 			System.out.println("Sol. Iterativa: " + Ejercicio1.ejercicio1It(l.get(0), l.get(1), l.get(2)));
 			System.out.println("Sol. Rec. No Final: " + Ejercicio1.ejercicio1RecNoFin(l.get(0), l.get(1), l.get(2), ""));
 			System.out.println("Sol. Rec. Final: " + Ejercicio1.ejercicio1RecFin(l.get(0), l.get(1), l.get(2)));
+			System.out.println("Sol. Funcional: " + Ejercicio1.ejercicio1Fun(l.get(0), l.get(1), l.get(2)));
 		}
 		System.out.println("==============================================================");
 		System.out.println("========================Ejercicio 2======================================");
